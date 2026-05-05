6888a12e23c75816bdaae64c7c17aac05360bf94
ujjwal3974-ctrl <ujjwal3974@gmail.com>
2026-05-06 02:44:14 +0530

    UC1: Basic TicTacToe Board display

---
+++ TicTacToe.java
@@ -0,0 +1,26 @@
+public class TicTacToe {
+
+    static char[][] board = new char[3][3];
+
+    public static void main(String[] args) {
+        initializeBoard();
+        printBoard();
+    }
+
+    static void initializeBoard() {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                board[row][col] = '-';
+            }
+        }
+    }
+
+    static void printBoard() {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                System.out.print(board[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
